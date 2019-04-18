49713a6b4670e046c7aaeabcb5d1ffe77be4f3a3
Andrew Heller <andrewhheller@gmail.com>
2019-04-17 23:59:35 -0400

    setup backend nodemailer

--- server/app.js
+++ server/app.js
@@ -4,13 +4,20 @@ const app = express();
 const bodyParser = require('body-parser');
 const path = require('path');
 
+// API routes
 const subRoute = require('./api/sub');
+const contactRoute = require('./api/contact');
+
+// ### MIDDLEWARE ###
 
 app.use(express.static('dist'));
-// app.use(express.static('node_modules'))
 app.use(bodyParser.json());
-app.use('/api/sub', subRoute);
 
+// APIR routes
+app.use('/api/sub', subRoute);
+app.use('/api/contact', contactRoute);
+
+// serve main page
 app.get('/', (req, res, next) => {
   res.sendFile(path.join(__dirname, '../index.html'))
 });

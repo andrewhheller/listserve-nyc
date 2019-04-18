2f890a93bcccf623233d4d9e810e3941bb1682b2
Andrew Heller <andrewhheller@gmail.com>
2019-04-17 21:45:10 -0400

    updated social media links

--- src/SocialNav.js
+++ src/SocialNav.js
@@ -4,8 +4,8 @@ const SocialNav = () => {
 
   return (
     <div id="logo">
-      <a href="https://www.facebook.com" target="_blank" className="fa fa-facebook fa-lg"></a>
-      <a href="https://www.twitter.com" target="_blank" className="fa fa-twitter fa-lg"></a>
+      <a href="https://www.facebook.com/andrewhheller" target="_blank" className="fa fa-facebook fa-lg"></a>
+      <a href="https://www.twitter.com/andrewhheller" target="_blank" className="fa fa-twitter fa-lg"></a>
       <p className="share-pipe">|</p>
       <a href="http://www.twitter.com/share?url=http://www.google.com/" target="_blank"className="tweet-button">Tweet</a>
     </div>

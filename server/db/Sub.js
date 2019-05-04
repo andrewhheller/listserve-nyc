5da99bfe3d2bfe5b2b20720ef7dff082e971dc0b
Andrew Heller <andrewhheller@gmail.com>
2019-05-03 20:00:37 -0400

    have winner API update subscribed user to winner and send back that winner

--- server/db/Sub.js
+++ server/db/Sub.js
@@ -34,8 +34,24 @@ Sub.genPool = () => {
 
 // generate winner
 Sub.winner = () => {
+
+  // invoke generate pool function
   return Sub.genPool()
-    .then(emails => emails[Math.floor(Math.random() * emails.length)])
+
+    .then(emails => {
+
+      // calculate winner (random)
+      const winner = emails[Math.floor(Math.random() * emails.length)];
+
+      // find winner by ID and update prize status to winner
+      Sub.findById(winner.id)
+        .then(sub => {
+           sub.update({ prize: 'winner' })
+         })
+
+      // return winner (will be sent back by API)
+      return winner;
+    })
 }
 
 

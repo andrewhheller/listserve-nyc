ea7ab9f6547e51b40047426b3a8f8939e2123ed5
Andrew Heller <andrewhheller@gmail.com>
2019-05-05 23:04:58 -0400

    basic email verify POST route

--- server/api/sub.js
+++ server/api/sub.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+const transporter = require('../utils/email');
+
+const FROM_EMAIL = process.env.FROM_EMAIL;
+
 const { Sub } = require('../db').models;
 
 router.get('/', (req, res, next) => {
@@ -23,7 +27,37 @@ router.post('/', (req, res, next) => {
   })
     .then(result => res.send(result))  // result is array where [0] = instance and [1] = wasCreated boolean
     .catch(error => next(error))
-})
+});
+
+router.post('/verify', (req, res, next) => {
+
+  const subscriberMail = req.body.email;
+  const message = 'Thanks for subscribing!';
+  
+  const mailOptions = {
+    from: FROM_EMAIL,
+    to: subscriberMail,
+    subject: 'listserve NYC Subscription',
+    text: message
+  }
+
+  transporter.sendMail(mailOptions, (error, data) => {
+
+    if(error) {
+      res.json({
+        msg: 'fail'
+      })
+    }
+
+    else {
+      res.json({
+        msg: 'success'
+      })
+    }
+
+  })
+
+});
 
 
 

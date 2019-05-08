558b5f31bcbc0328fca96550af1cddba1a00dbd0
Andrew Heller <andrewhheller@gmail.com>
2019-05-07 22:12:31 -0400

    configured express for 404 and 500 error handling)

--- server/app.js
+++ server/app.js
@@ -11,9 +11,11 @@ const path = require('path');
 const subRoute = require('./api/sub');
 const contactRoute = require('./api/contact');
 
+// pages
+const index = path.join(__dirname, '../dist/index.html');
+const errorPage = path.join(__dirname, '../dist/404.html');
+
 // ### MIDDLEWARE ###
-
-
 
 app.use(express.static('dist'));
 app.use(bodyParser.json());
@@ -25,9 +27,18 @@ app.use('/api/contact', contactRoute);
 
 // serve main page
 app.get('/', (req, res, next) => {
-  res.sendFile(path.join(__dirname, '../index.html'))
+  res.sendFile(path.join(__dirname, index))
 });
 
+// error handling
+app.use((req, res, next) => {
+  res.status(404).sendFile(errorPage)
+});
+
+app.use((error, req, res, next) => {
+  console.log(error);
+  res.status(500).send('<h1>An error has occurred</h1>');
+});
 
 
 module.exports = app;

c38d9d44da205ad706fa5df0cafe9503b9716e25
Andrew Heller <andrewhheller@gmail.com>
2019-05-27 19:50:15 -0400

    fixed space before email in modal

--- src/Modal.js
+++ src/Modal.js
@@ -13,8 +13,8 @@ const Modal = ({ show, modalEmail, handleClose }) => {
     	<div className="modal-content">
 		    <div className="modal-content-text">
       		<h2>Great job!</h2>
-      		<p>We just emailed a confirmation link to 
-							<span>{ modalEmail }</span>.
+      		<p>We just emailed a confirmation link to
+							<span> { modalEmail }</span>.
 							<br />
 						Click the link to confirm your subscription!</p>
       		<button type="button" className="closeBtn" onClick={ handleClose }>OK</button>
